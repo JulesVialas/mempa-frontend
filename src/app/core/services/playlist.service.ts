ee30f10bd7f79a6b3f324b880a2118075f7bd9be
Jules Vialas <jules.vialas@gmail.com>
2026-02-05 22:18:52 +0100

    ajout du service Playlist et de son test associé

---
+++ src/app/core/services/playlist.service.ts
@@ -0,0 +1,33 @@
+/**
+ * playlist.service.ts       05/02/2026
+ * MIAGE de Toulouse, pas de copyright
+ */
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Playlist } from '../models/playlist.model';
+import { environment } from '../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+/**
+ * Service pour gérer les opérations liées aux playlists
+ */
+export class PlaylistService {
+
+  /** URL de l'API */
+  private apiUrl = `${environment.apiUrl}/playlists`;
+
+  /** Constructeur */
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Récupère la liste des playlists depuis l'API
+   */
+  getPlaylists(): Observable<Playlist[]> {
+    return this.http.get<Playlist[]>(this.apiUrl);
+  }
+}
