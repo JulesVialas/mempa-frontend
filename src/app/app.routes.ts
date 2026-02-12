3df40783f1827c1d92f2823358936b18827f83da
Jules Vialas <jules.vialas@gmail.com>
2026-02-12 22:36:16 +0100

    feat(routing): ajout de la route pour la page de détails - Ajout de la route /playlist/:id vers PlaylistDetail - Redirection par défaut vers /dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,17 @@
+/**
+ * app.routes.ts        05/02/2026
+ * MIAGE de Toulouse, pas de copyright
+ */
+
 import { Routes } from '@angular/router';
 import { Dashboard } from './features/dashboard/dashboard';
+import { PlaylistDetail } from './features/playlist-detail/playlist-detail';
 
+/**
+ * Configuration des routes de l'application
+ */
 export const routes: Routes = [
-  { path: 'dashboard', component: Dashboard }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'playlist/:id', component: PlaylistDetail }
 ];
