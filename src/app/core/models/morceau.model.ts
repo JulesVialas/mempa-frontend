fb8acc08896ef2eb9eb5a6b604a23f35381b8ce7
Jules Vialas <jules.vialas@gmail.com>
2026-02-12 22:35:10 +0100

    feat(core): ajout du modèle Morceau - Création de l'interface Morceau (id, titre, artiste) - Représente les morceaux de musique dans une playlist

---
+++ src/app/core/models/morceau.model.ts
@@ -0,0 +1,13 @@
+/**
+ * morceau.model.ts       12/02/2026
+ * MIAGE de Toulouse, pas de copyright
+ */
+
+/**
+ * Interface représentant un morceau
+ */
+export interface Morceau {
+    id: number;
+    titre: string;
+    artiste: string;
+}
