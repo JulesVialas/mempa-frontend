4b1f51010ca166334294332061d82fb443aea230
Jules Vialas <jules.vialas@gmail.com>
2026-02-12 22:35:38 +0100

    feat(core): extension du service Playlist - Ajout de la méthode getPlaylistById() pour récupérer une playlist par son ID - Ajout de la méthode getContenuPlaylist() pour récupérer les morceaux d'une playlist - Import du modèle Morceau - Mise à jour des tests unitaires

--- src/app/core/services/playlist.service.ts
+++ src/app/core/services/playlist.service.ts
@@ -7,27 +7,44 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Playlist } from '../models/playlist.model';
-import { environment } from '../../../environments/environment.development';
-
-@Injectable({
-  providedIn: 'root'
-})
+import { environment } from '../../../environments/environment';
+import {Morceau} from "../models/morceau.model";
 
 /**
  * Service pour gérer les opérations liées aux playlists
  */
+@Injectable({
+  providedIn: 'root'
+})
 export class PlaylistService {
 
-  /** URL de l'API */
-  private apiUrl = `${environment.apiUrl}/playlists`;
+  private readonly apiUrl = `${environment.apiUrl}/playlists`;
 
-  /** Constructeur */
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   /**
    * Récupère la liste des playlists depuis l'API
+   * @returns Observable contenant la liste des playlists
    */
   getPlaylists(): Observable<Playlist[]> {
     return this.http.get<Playlist[]>(this.apiUrl);
+  }
+
+  /**
+   * Récupère une playlist par son ID
+   * @param id Identifiant de la playlist
+   * @returns Observable contenant la playlist
+   */
+  getPlaylistById(id: number): Observable<Playlist> {
+    return this.http.get<Playlist>(`${this.apiUrl}/${id}`);
+  }
+
+  /**
+   * Récupère les morceaux d'une playlist
+   * @param playlistId Identifiant de la playlist
+   * @returns Observable contenant la liste des morceaux
+   */
+  getContenuPlaylist(playlistId: number): Observable<Morceau[]> {
+    return this.http.get<Morceau[]>(`${this.apiUrl}/${playlistId}/morceaux`);
   }
 }
--- src/app/core/services/playlist.spec.ts
+++ src/app/core/services/playlist.spec.ts
@@ -1,12 +1,20 @@
+/**
+ * playlist.spec.ts        05/02/2026
+ * MIAGE de Toulouse, pas de copyright
+ */
+
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 import { PlaylistService } from './playlist.service';
 
-describe('Playlist', () => {
+describe('PlaylistService', () => {
   let service: PlaylistService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient()]
+    });
     service = TestBed.inject(PlaylistService);
   });
 
